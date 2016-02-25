f73252b5ce9c829f73ed377357568c303fac7c9b
EvilDmitri <dmitribrach@gmail.com>
2016-02-25 21:06:34 +0200

    first row

--- scraper.py
+++ scraper.py
@@ -4,7 +4,6 @@ import csv
 import logging
 import os
 
-
 from grab.spider import Spider, Task
 from grab.tools import html
 
@@ -12,6 +11,7 @@ from grab.tools.logs import default_logging
 from hashlib import sha1
 
 from grab import Grab
+
 g = Grab()
 
 default_logging(level=logging.DEBUG)
@@ -27,6 +27,9 @@ class Immospider(Spider):
     def __init__(self):
         super(Immospider, self).__init__(thread_number=THREADS, network_try_limit=20)
         self.result_file = csv.writer(open('result.txt', 'w'))
+        self.result_file.writerow(['Title', 'Address', 'Wohnungstyp', 'Etage', 'Wohnflaeche', 'Bezugsfrei_ab',
+                                  'Zimmer', 'Haustiere', 'Kaltmiete', 'Nebenkosten', 'Heizkosten', 'Gesamtmiete',
+                                  'Kaution_o_genossenschaftsanteile'])
 
     def task_generator(self):
 
@@ -35,7 +38,8 @@ class Immospider(Spider):
         a = g.xpath_list('//select[@class="select font-standard"]')[0]
         pages = len(a.getchildren())
 
-        for number in xrange(pages):
+        for number in xrange(10):
+        # for number in xrange(pages+1):
             url = MAIN_LINK.format(number)
             yield Task('initial', url=url)
 
@@ -47,8 +51,25 @@ class Immospider(Spider):
             self.add_task(Task(name='get_data', url=url))
 
     def task_get_data(self, grab, task):
-        name = task.image_name
-        grab.response.save(name, create_dirs=True)
+        title = grab.doc.select('//h1[@id="expose-title"]')[0].text()
+        address = grab.doc.select('//div[@class="address-block"]')[0].text()
+        address = ''.join(address.split(' (zur Karte)'))    # garbage text
+
+        wohnungstyp = grab.doc.select('//dd[@class="is24qa-wohnungstyp grid-item three-fifths"]')[0].text()
+        etage = grab.doc.select('//dd[@class="is24qa-etage grid-item three-fifths"]')[0].text()
+        wohnflaeche = grab.doc.select('//dd[@class="is24qa-wohnflaeche-ca grid-item three-fifths"]')[0].text()
+        bezugsfrei_ab = grab.doc.select('//dd[@class="is24qa-bezugsfrei-ab grid-item three-fifths"]')[0].text()
+        zimmer = grab.doc.select('//dd[@class="is24qa-zimmer grid-item three-fifths"]')[0].text()
+        haustiere = grab.doc.select('//dd[@class="is24qa-haustiere grid-item three-fifths"]')[0].text()
+        kaltmiete = grab.doc.select('//dd[@class="is24qa-kaltmiete grid-item three-fifths"]')[0].text()
+        nebenkosten = grab.doc.select('//dd[@class="is24qa-nebenkosten grid-item three-fifths"]')[0].text()
+        heizkosten = grab.doc.select('//dd[@class="is24qa-heizkosten grid-item three-fifths"]')[0].text()
+        gesamtmiete = grab.doc.select('//dd[@class="is24qa-gesamtmiete grid-item three-fifths font-bold"]')[0].text()
+        kaution_o_genossenschaftsanteile = grab.doc.select(
+            '//dd[@class="is24qa-kaution-o-genossenschaftsanteile is24-ex-spacelink grid-item three-fifths"]')[0].text()
+
+        self.result_file.writerow([title, address, wohnungstyp, etage, wohnflaeche, bezugsfrei_ab, zimmer, haustiere, kaltmiete,
+                                  nebenkosten, heizkosten, gesamtmiete, kaution_o_genossenschaftsanteile])
 
 
 def main():
